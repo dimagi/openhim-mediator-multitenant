3812f0656f40f3a1696085082a1020a3741d128c
Norman Hooper <nhooper@dimagi.com>
2019-01-28 18:45:00 +0200

    Auth settings for upstream service

--- mediator/mediator/settings.py
+++ mediator/mediator/settings.py
@@ -73,10 +73,26 @@ MEDIATOR_CONF = {
             'description': 'The URL of the service upstream of the mediator',
             'type': 'string',
             'template': [],
+        },
+        {
+            'param': 'upstreamUsername',
+            'displayName': 'Upstream username',
+            'description': 'The username to authenticate with the upstream service',
+            'type': 'string',
+            'template': [],
+        },
+        {
+            'param': 'upstreamPassword',
+            'displayName': 'Upstream password',
+            'description': 'The password to authenticate with the upstream service',
+            'type': 'string',
+            'template': [],
         }
     ],
     'config': {
         'upstreamUrl': os.environ['MEDIATOR_UPSTREAM_URL'],
+        'upstreamUsername': os.environ['MEDIATOR_UPSTREAM_USERNAME'],
+        'upstreamPassword': os.environ['MEDIATOR_UPSTREAM_PASSWORD'],
     }
 }
 
