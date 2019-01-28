7bc87838bf00f1ac99fcb4ca19b7d44c9bc69e48
Norman Hooper <nhooper@dimagi.com>
2019-01-28 19:23:01 +0200

    Forward request upstream

--- mediator/passthru/views.py
+++ mediator/passthru/views.py
@@ -1,7 +1,9 @@
+import json
 from datetime import datetime
 
+import requests
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import JsonResponse, QueryDict
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -22,11 +24,50 @@ def get_http_headers(request_meta):
     return headers
 
 
-def get_body_string(request):
-    """
-    Return request body as a string (as opposed to bytes
-    """
-    return request.body.decode(request.encoding) if request.body else ''
+def forward_request_upstream(request):
+    upstream_url = settings.MEDIATOR_CONF['config']['upstreamUrl']
+    upstream_username = settings.MEDIATOR_CONF['config']['upstreamUsername']
+    upstream_password = settings.MEDIATOR_CONF['config']['upstreamPassword']
+
+    url = '/'.join((upstream_url.rstrip('/'), request.path.lstrip('/')))
+    query_string = request.META['QUERY_STRING']
+    body = request.body.decode(request.encoding) if request.body else ''
+    try:
+        data = None
+        json_data = json.loads(body)
+    except json.JSONDecodeError:
+        data = body
+        json_data = None
+    headers = get_http_headers(request.META)
+    request_ts = datetime.utcnow()
+    response = requests.request(
+        request.method.lower(),
+        url,
+        params=QueryDict(query_string),
+        data=data,
+        json=json_data,
+        headers=headers,
+        auth=(upstream_username, upstream_password)
+    )
+    response_ts = datetime.utcnow()
+
+    return {
+        'name': 'Primary Route',
+        'request': {
+            'method': request.method,
+            'headers': headers,
+            'body': body,
+            'timestamp': str(request_ts),
+            'path': request.path,
+            'querystring': query_string,
+        },
+        'response': {
+            'status': response.status_code,
+            'headers': dict(response.headers),
+            'body': response.content.decode(response.encoding),
+            'timestamp': str(response_ts),
+        }
+    }
 
 
 # Extend the mediator by adding more routes/views
@@ -36,30 +77,9 @@ def primary_route(request):
     The passthrough mediator forwards the incoming request unchanged
     """
     # Extend the route by building out more complex orchestrations
-    orchestrations = [{
-        'name': 'Primary Route',
-        'request': {
-            'method': request.method,
-            'headers': get_http_headers(request.META),
-            'body': get_body_string(request),
-            'timestamp': str(datetime.utcnow()),
-            'path': request.path,
-            'querystring': request.META['QUERY_STRING'],
-        },
-        'response': {
-            'status': 200,
-            'headers': {'Content-Type': 'application/json'},
-            'body': 'Primary Route Reached',
-            'timestamp': str(datetime.utcnow()),
-        }
-    }]
+    orchestrations = [forward_request_upstream(request)]
+    primary_route_response = orchestrations[0]['response']
 
-    primary_route_response = {
-        'status': 200,
-        'headers': {'content-type': 'application/json'},
-        'body': 'Primary Route Reached',
-        'timestamp': str(datetime.utcnow()),
-    }
     data = {
         'x-mediator-urn': settings.MEDIATOR_CONF['urn'],
         'status': 'Successful',
